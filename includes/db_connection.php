1b3b2559c053e6e8f5aa41c50b9e0415e02c8b95
Emiliano Ortiz <tsurustuneadosemiliano@gmail.com>
2026-01-12 20:22:35 -0600

    act comentarios

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,21 +1,17 @@
 <?php
 $host = "localhost";
 $port = "5432";
-$dbname = "sistema_mascotas"; // ¡CAMBIA ESTO!
-$user = "postgres"; // ¡CAMBIA ESTO!
-$password = "potros26"; // ¡CAMBIA ESTO!
+$dbname = "sistema_mascotas"; // ¡Modifiquen este ya que es le nombre de la base de datos!
+$user = "postgres"; 
+$password = "potros26"; // ¡contrasena de postresql!
 
 try {
-    // Cadena de conexión DSN
     $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
     
-    // Crear una instancia de PDO para la conexión
     $pdo = new PDO($dsn, $user, $password);
     
-    // Configurar PDO para que lance excepciones en caso de error
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    // Si la conexión falla, detiene la ejecución y muestra el error
     die("Error de conexión a la base de datos: " . $e->getMessage());
 }
 ?>
